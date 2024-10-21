48257e709ae58cea1d42809cc0c915619fcfd560
BwesewB <sebastianfok@gmail.com>
2024-10-21 11:15:12 -0700

    added a few styling

--- frontendAssignments/Week8/src/COmponents/Filter.jsx
+++ frontendAssignments/Week8/src/COmponents/Filter.jsx
@@ -1,10 +1,12 @@
+import './App.css'
+
 export default function Filter({ setFilter }) {
     return (
         <>
             <div>
-                <button onClick={() => setFilter('all')}>All</button>
-                <button onClick={() => setFilter('completed')}>Completed</button>
-                <button onClick={() => setFilter('pending')}>Pending</button>
+                <button onClick={() => setFilter('all')} className="buttonFilter">All</button>
+                <button onClick={() => setFilter('completed')} className="buttonFilter">Completed</button>
+                <button onClick={() => setFilter('pending')} className="buttonFilter">Pending</button>
             </div>
         </>
     )
